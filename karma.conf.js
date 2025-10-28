2732ecbc087006ffc5a1406e9109a1887a0313c2
Conny González <119901329+sparklyeonn@users.noreply.github.com>
2025-10-27 22:19:38 -0300

    fix: karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,40 +1,64 @@
 module.exports = function (config) {
     config.set({
 
-        frameworks: ['jasmine'],
+        frameworks: ['jasmine', 'webpack'], 
 
         files: [
-            'src/**/*.test.js' 
+            'src/**/*.test.js',
+            'src/**/*.spec.js'
+        ],
+
+        exclude: [
+            'node_modules'
         ],
 
         preprocessors: {
-            'src/**/*.test.js': ['webpack']
+            'src/**/*.test.js': ['webpack'],
+            'src/**/*.spec.js': ['webpack']
         },
 
         webpack: {
+            mode: 'development', 
             module: {
                 rules: [
                     {
-                        test: /\.jsx?$/,
-                        exclude: /node_modules/,
+                        test: /\.(js|jsx)$/,
+                        exclude: /node_modules/, 
                         use: {
                             loader: 'babel-loader',
                             options: {
-                                presets: ['@babel/preset-react', '@babel/preset-env']
+                                presets: ['@babel/preset-env', '@babel/preset-react']
                             }
                         }
-                    }
+                    },
                 ]
             },
             resolve: {
                 extensions: ['.js', '.jsx']
-            }
+            },
+            devtool: 'inline-source-map'
         },
+
+        webpackMiddleware: {
+            stats: 'errors-only'
+        },
+
+        reporters: ['progress'],
+
+        port: 9876,
+
+        colors: true,
+
+        logLevel: config.LOG_INFO,
+
+        autoWatch: true,
 
         browsers: ['Chrome'],
 
-        reporters: ['progress'],
-        
-        singleRun: false
+        singleRun: false,
+
+        browserNoActivityTimeout: 60000, // 60 segundos
+
+        concurrency: Infinity
     });
 };
