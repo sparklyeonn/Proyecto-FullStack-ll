12026e512f3c5c49eca0916f00da72fd5c1eb1e7
Alejandra González <aleja.gonzalezc@duocuc.cl>
2025-12-15 23:45:19 -0300

    fix: logica de navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-// src/App.jsx
 import React from "react";
 import "./styles.css";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -16,11 +15,8 @@ import ProductoDetalle from "./pages/ProductoDetalle";
 import Registro from "./pages/Registro";
 import Carrito from "./pages/Carrito";
 
-// guards
-import AdminRoute from "./components/AdminRoute";
 import PrivateRoute from "./components/PrivateRoute";
-
-// admin
+import AdminRoute from "./components/AdminRoute";
 import AdminLayout from "./components/AdminLayout";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import AdminProductos from "./pages/admin/AdminProductos";
@@ -36,39 +32,32 @@ function Layout() {
   );
 }
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* PUBLICO + LAYOUT */}
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Inicio />} />
-          <Route path="cds" element={<Cds />} />
-          <Route path="contacto" element={<Contacto />} />
-          <Route path="accesorios" element={<Accesorios />} />
-          <Route path="vinilos" element={<Vinilos />} />
-          <Route path="login" element={<Login />} />
-          <Route path="producto/:id" element={<ProductoDetalle />} />
-          <Route path="registro" element={<Registro />} />
-          <Route path="carrito" element={<Carrito />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Inicio />} />
+        <Route path="cds" element={<Cds />} />
+        <Route path="contacto" element={<Contacto />} />
+        <Route path="accesorios" element={<Accesorios />} />
+        <Route path="vinilos" element={<Vinilos />} />
+        <Route path="login" element={<Login />} />
+        <Route path="producto/:id" element={<ProductoDetalle />} />
+        <Route path="registro" element={<Registro />} />
+        <Route path="carrito" element={<Carrito />} />
 
-          {/* PERFIL */}
-          <Route element={<PrivateRoute />}>
-            <Route path="perfil" element={<Perfil />} />
-          </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="perfil" element={<Perfil />} />
+        </Route>
 
-          {/* ADMIN */}
-          <Route element={<AdminRoute />}>
-            <Route path="admin" element={<AdminLayout />}>
-              <Route index element={<AdminDashboard />} />
-              <Route path="productos" element={<AdminProductos />} />
-              <Route path="usuarios" element={<AdminUsuarios />} />
-            </Route>
+        <Route element={<AdminRoute />}>
+          <Route path="admin" element={<AdminLayout />}>
+            <Route index element={<AdminDashboard />} />
+            <Route path="productos" element={<AdminProductos />} />
+            <Route path="usuarios" element={<AdminUsuarios />} />
           </Route>
         </Route>
-      </Routes>
-    </BrowserRouter>
+      </Route>
+    </Routes>
   );
 }
-
-export default App;
