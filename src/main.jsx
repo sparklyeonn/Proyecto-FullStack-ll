0588b8c35a9454a08b39f5b95e3fa4f7a09dc622
Alejandra González <aleja.gonzalezc@duocuc.cl>
2025-12-16 00:25:48 -0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 import App from "./App.jsx";
 import { AppProvider } from "./context/AppContext.jsx";
 
